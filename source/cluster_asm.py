c2b7d338607d3d2b0b3e36c32063e1e9dc6ea423
jgreer013 <jeremiahgreer013@gmail.com>
2019-01-17 07:58:59 -0500

    Added full clusters

--- source/cluster_asm.py
+++ source/cluster_asm.py
@@ -8,26 +8,39 @@ import seaborn as sns
 from gensim.models import Word2Vec
 from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
 from sklearn.cluster import DBSCAN
+import pickle
 
 
 def main():
-    fl = FileLoader()
-    dir = "source/cpp_examples/assembly/"
-    files = os.listdir("source/cpp_examples/assembly/")
-    for f in files:
-        if f[-8:] == "only.txt":
-            fl.addFilename(dir + f)
+    try:
+        print("Loading data from pickle")
+        fp = open("data_pickle.obj",'rb')
+        d = pickle.load(fp, encoding='unicode_')
+    except:
+        print("Pickle loading failed")
+        print("Loading files manually")
+        fl = FileLoader()
+        dir = "source/cpp_examples/assembly/"
+        files = os.listdir("source/cpp_examples/assembly/")
+        for f in files:
+            if f[-8:] == "only.txt":
+                fl.addFilename(dir + f)
 
+        d = fl.getData(type=np.dtype('unicode_'))
+        print("Pickling data for faster loading")
+        fp = open('data_pickle.obj','wb')
+        pickle.dump(d, fp)
+        fp.close()
+        return
     uniques = set()
-    d = fl.getData(type=np.dtype('unicode_'))
     sentences = []
     for data in d:
         uniques.update(data[1])
         sentences.append(list(data[1]))
 
     instr = sorted(list(uniques))
-    size = 300
-    window = 3
+    size = 500
+    window = 5
     model = Word2Vec(sentences, size=size, window=window, workers=4, sg=0, min_count=1)
     vocab = list(model.wv.vocab)
     print(uniques)
@@ -45,19 +58,20 @@ def main():
                 k += vocab[j] + ", "
         print(k[:-2])
     """
-    md = 0.2
+    md = 5.0
     plt.figure(figsize=(10,7))
     Z = linkage(X, method='ward', metric='euclidean')
     dend = dendrogram(Z, labels=labels)
     plt.axhline(y=md, c='k')
     plt.title('Euclidean Ward Dendrogram of Assembly Commands')
     clusters = fcluster(Z, md, criterion='distance')
-    #print(clusters)
-    #plt.show()
-    #print(len(vocab))
-    #print(len(clusters))
+    print(clusters)
+    plt.show()
+    print(len(vocab))
+    print(len(clusters))
+    print(len(set(clusters)))
 
-    f = open("clusters.txt", 'w')
+    f = open("clusters_full.txt", 'w')
     for i in range(len(clusters)):
         term = vocab[i]
         clus = str(clusters[i])
