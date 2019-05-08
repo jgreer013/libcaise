aa293ccae6412c6a79b855aabf76e6cadd5d0bb0
Jeremiah Greer <JeremiahGreer013@gmail.com>
2019-05-08 08:36:33 -0400

    Added linear swap search trojan, updated files

--- source/cpp_examples/get_dynamic.sh
+++ source/cpp_examples/get_dynamic.sh
@@ -4,10 +4,9 @@ for f in ./bin/*.b
 do
   b=$(basename $f)
   f2=${b::${#b}-2}
-  f2=$f2"_dynamic_no_library.txt"
-  echo ./dynamic_nol/$f2
-  rm ./dynamic_nol/$f2
-  gdb $f < run_no_library.gdb > ./dynamic_nol/$f2
+  f2=$f2"_dynamic.txt"
+  echo ./dynamic/$f2
+  rm ./dynamic/$f2
+  gdb $f < run.gdb > ./dynamic/$f2
   #grep -Po "$s2" > ./assembly/$f2
-  break
 done
---
+++ source/cpp_examples/run.gdb
@@ -0,0 +1,9 @@
+b _start
+run
+
+while 1
+  x/i $pc
+  stepi
+end
+
+quit
